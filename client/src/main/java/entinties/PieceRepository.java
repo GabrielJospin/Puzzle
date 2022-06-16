21493e02d96065f848131eb6b1d24ac6834f86d3
Gabriel Jospin <gabriel.jospin@usp.br>
2022-06-16 17:18:15 -0300

    Fix Part implemention in client

---
+++ client/src/main/java/entinties/PieceRepository.java
@@ -0,0 +1,45 @@
+package entinties;
+
+import stubs.Part;
+import stubs.PartRepository;
+
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class PieceRepository implements PartRepository {
+
+    List<Part> partList;
+
+    public PieceRepository() {
+        this.partList = new ArrayList<>();
+    }
+
+    @Override
+    public List<Part> getPartList() throws RemoteException {
+        return this.partList;
+    }
+
+    @Override
+    public void addPart(Part part) throws RemoteException {
+        this.partList.add(part);
+    }
+
+    @Override
+    public Part addPart(String name, String description) throws RemoteException {
+        Part part = new PartPiece(name, description);
+        this.partList.add(part);
+        return part;
+    }
+
+    @Override
+    public void addSubComponent(Part part, Part subpart, int quant) {
+
+    }
+
+    @Override
+    public Part getPart(UUID id) {
+        return null;
+    }
+}
