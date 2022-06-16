cc4b434674ab568313d12dcf753bcc1d86d154ad
Gabriel Jospin <gabriel.jospin@usp.br>
2022-06-16 16:45:06 -0300

    create PartRepository interface

---
+++ client/src/main/java/stubs/PartRepository.java
@@ -0,0 +1,16 @@
+package stubs;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.List;
+import java.util.UUID;
+
+public interface PartRepository extends Remote {
+
+    List<Part> getPartList() throws RemoteException;
+    void addPart(Part part) throws RemoteException;
+    Part addPart(String name, String description) throws RemoteException;
+    void addSubComponent(Part part, Part subpart, int quant);
+    Part getPart(UUID id);
+
+}
